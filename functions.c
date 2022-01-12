f5f958f72e3dec3af224e8616256861f036c3a29
Diegoacosta127 <diegoacosta127@gmail.com>
2022-01-12 05:51:15 -0800

    some functiones added

--- functions.c
+++ functions.c
@@ -5,7 +5,6 @@ void push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *new = malloc(sizeof(stack_t));
 
-	(void)line_number;
 	if (!new)
 	{
 		return (NULL);
@@ -13,6 +12,11 @@ void push(stack_t **stack, unsigned int line_number)
 	new->n = atoi(strtok(NULL, " "));
 	new->prev = NULL;
 	new->next = NULL;
+	if (!(new->n))
+	{
+		fprintf(stderr, "L%u: usage: push integer", line_number);
+		exit(EXIT_FAILURE);
+	}
 	if (!(*stack))
 	{
 		*stack = new;
@@ -28,9 +32,10 @@ void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *aux = *stack;
 
+	(void)line_number;
 	if (!aux)
 	{
-		fprintf(stderr, "Error")
+		fprintf(stderr, "Can't pall")
 		exit(EXIT_FAILURE);
 	}
 	while (aux)
@@ -39,3 +44,48 @@ void pall(stack_t **stack, unsigned int line_number)
 		aux = aux->next;
 	}
 }
+/**
+  */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (!stack)
+	{
+		fprintf(stderr, "L%u: can't pint", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d", *(stack)->n;);
+}
+/**
+  */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	if(!(*stack))
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack", line_number);
+		exit(EXIT_FAILURE);
+	}
+	*stack = *(stack)->next;
+	free(*(stack)->prev);
+	*(stack)->prev = NULL;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
